80a74702a8d2afefafeffa50ed07dc56814cfd94
TBernard97 <darby.bernard@gmail.com>
2020-08-24 12:09:07 -0400

    Update Remote_Register_Id.ps1
    Added 1909 and 1903 ID's.

--- Remote_Register_Id.ps1
+++ Remote_Register_Id.ps1
@@ -1,5 +1,7 @@
 $file = Read-Host -Prompt "Input Target List Path"
 $computers = Get-Content -Path $file 
+$1909 = Read-Host -Prompt "Input File Name for 1909"
+$1903 = Read-Host -Prompt "Input File Name for 1903"
 $1809 = Read-Host -Prompt "Input File Name for 1809"
 $1803 = Read-Host -Prompt "Input File Name for 1803"
 $1709 = Read-Host -Prompt "Input File Name for 1709"
@@ -8,18 +10,27 @@ $1607 = Read-Host -Prompt "Input File Name for 1607"
 $Other = Read-Host -Prompt "Input File Name for Other Register ID's"
 $Offline = Read-Host -Prompt "Input File Name for Offline Nodes"
 
+$ErrorActionPreference = 'silentlycontinue'
 
 foreach($computer in $computers){
     if(Test-Connection -computername $computer -Quiet){
         $Hive = [Microsoft.Win32.RegistryHive]::LocalMachine
         $KeyPath = 'SOFTWARE\Microsoft\Windows NT\CurrentVersion'
         $Value = 'ReleaseId'
+        $os = Get-WmiObject Win32_OperatingSystem -Computer $computer
+        $osversion = $os.version 
         
         $reg = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey($Hive, $computer)
         $key = $reg.OpenSubKey($KeyPath)
         
         $keystring = $key.GetValue($Value)
-        Write-Output("$computer -> $keystring")
+        
+        if($osversion -eq '6.1.7601'){
+           Write-Output("$computer -> Win 7") 
+        } else{
+            Write-Output("$computer -> $keystring")
+        }
+        
         
         if($keystring -match "1803"){
             Write-Output($computer) | Out-File -Append $1803
@@ -36,10 +47,17 @@ foreach($computer in $computers){
         elseif($keystring -match "1607"){
             Write-Output($computer) | Out-File -Append $1607
         }
-        else{
+        elseif($osversion -eq '6.1.7601'){
             Write-Output($computer) | Out-File -Append $Other
         }
         
+        elseif($keystring -match "1903"){
+            Write-Output($computer) | Out-File -Append $1903
+        } 
+
+        elseif($keystring -match "1909"){
+            Write-Output($computer) | Out-File -Append $1909
+        }
         
     }
 
